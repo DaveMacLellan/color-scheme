ff5a7df359a305866d9d036ce456d21926a8efa5
DaveMacLellan <davemaclellanx@gmail.com>
2022-05-03 21:40:20 -0400

    Replaced fixed hex codes
    Deleted hex codes from html. Added hex codes and elements in javascript

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ function applyColors() {
     const colorChoice = document.getElementById("color-picker").value.substring(1)
     const schemeSelect = document.getElementById("scheme-select").value
 
+    let hexCol = document.getElementById("hexCodes")
+    hexCol.innerHTML = ""
     let colorArray = []
+
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorChoice}&mode=${schemeSelect}&count=5`, {method: "GET"})
     .then(res => res.json())
     .then(data => {    
         colorArray = data.colors
         for(let i = 0; i < colorArray.length; i++){
             document.getElementById(`col${i + 1}`).style.backgroundColor = colorArray[i].hex.value
+            hexCol.innerHTML += `<p>${colorArray[i].hex.value}</p>`
         }
     })    
 }
